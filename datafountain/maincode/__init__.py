c9aa1c30977674989a6804574d9920927375c478
jianngtao0826 <2290055070@qq.com>
2019-09-26 15:57:16 +0800

    diyici

---
+++ datafountain/maincode/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2019/9/25 11:18 
+# @Author : Tao Jiang 
+# @File : __init__.py.py 
+# @Software: PyCharm
---
+++ datafountain/maincode/jinrongneginfo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2019/9/25 11:19 
+# @Author : Tao Jiang 
+# @File : jinrongneginfo.py 
+# @Software: PyCharm
+
